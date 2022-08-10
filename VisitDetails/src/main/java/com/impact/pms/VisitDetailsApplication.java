7ae5b9e1c5505d36e88b38b63d6d322ac63b9711
Krishna <krishs09@yahoo.in>
2022-08-10 12:19:38 +0530

    PMS krishna's module

---
+++ VisitDetails/src/main/java/com/impact/pms/VisitDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.impact.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitDetailsApplication.class, args);
+	}
+
+}
